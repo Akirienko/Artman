f8de1e2e076daedd16e78aa40b2e6720c36f475d
@Akirienko <lakirienkol@gmail.com>
2023-08-22 21:59:10 +0300

    created mobile menu

---
+++ components/MainTextComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>
+      <slot name="undertitle"></slot>
+    </span>
+    <h1>
+      <slot name="title"></slot>
+    </h1>
+    <p>
+      <slot name="subtitle"></slot>
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Home
+    <NuxtLink to="/about">About</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
